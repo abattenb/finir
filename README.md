b8a0e6797a10a14f0a80aff2748a0eb6fdf55800
Andrew Battenburg <abattenb@gmail.com>
2017-12-31 23:44:39 -0800

    Add today

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # finir
 
-> A Vue.js project
+> A completion app.
+
+fenir mean 'to finish'. This is a personal completion app, customized to my daily needs.
+
+## Goals
+
+* Color as a motivator
+* [IKEA Effect](https://en.wikipedia.org/wiki/IKEA_effect) + 'Yet Another Todo'
+* Forgiveness-as-a-Service
+
+## Concepts
+
+* Every day brings a new color, a new chance
+* Every day should be completable
+* Streaks are a *reflection* of the reward, not the 'reward' itself
+* Anti-data: you cannot interact with the past
+
+## Thoughts
+
+It's amazing how different an experience can be when you aren't mining data..
+
+A todo list is the developer equivalent of am industrial designer's chair..
+
+Naming concepts frames everything. 'Challenges' vs 'Todos'..
 
 ## Build Setup
 
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="timeline">
     <div 
-      v-for="day in data"
+      :style="styles(timeline[0])"
+      class="day today">
+      <div>
+        <div>
+          {{timeline[0].date}}
+        </div>
+        <div class="list">
+          <div 
+            v-for="challenge in list"
+            :key="challenge.thing"
+            class="challenge">
+            <i class="far fa-square"></i>
+            &nbsp;
+            {{ challenge.name }}
+          </div>
+        </div>
+        <button class="fin">
+          FIN
+        </button>
+      </div>
+    </div>
+    <div 
+      v-for="day in notToday"
       :key="day.date"
-      :style="background(day)"
+      :style="styles(day)"
       class="day">
       <div>
         <div>
           {{day.date}}
         </div>
-        <div>
-          {{day.complete}}
+        <div class="icon">
+          <i class="far fa-check-circle" v-if="day.complete"></i>
+          <i class="far fa-times" v-else></i>
         </div>
       </div>
     </div>
@@ -25,7 +48,12 @@ export default {
   name: 'timeline',
   data() {
     return {
-      data: [
+      list: [
+        { name: 'Wake up', done: false },
+        { name: 'Eat', done: false },
+        { name: 'Meditate', done: false },
+      ],
+      timeline: [
         {
           date: '2017-12-31',
           complete: true,
@@ -33,12 +61,12 @@ export default {
         },
         {
           date: '2017-12-30',
-          complete: false,
+          complete: true,
           color: '#26A69A',
         },
         {
           date: '2017-12-29',
-          complete: true,
+          complete: false,
           color: '#7E57C2',
         },
         {
@@ -48,7 +76,7 @@ export default {
         },
         {
           date: '2017-12-27',
-          complete: false,
+          complete: true,
           color: '#FFA726',
         },
       ],
@@ -57,11 +85,15 @@ export default {
   created() {
   },
   methods: {
-    background(day) {
-      const opacity = (day.complete ? 1.0 : 0.2);
+    styles(day) {
+      const opacity = (day.complete ? 1.0 : 0.1);
       const color = hexToRGB(day.color);
-      console.log(color);
       return `background-color: rgba(${color},${opacity})`;
+    },
+  },
+  computed: {
+    notToday() {
+      return this.timeline.slice(1, this.timeline.length);
     },
   },
 };
@@ -81,25 +113,70 @@ export default {
   display: flex;
   position: relative;
   height: 100%;
-  font-size: 3rem;
-  font-weight: 900;
-  color: white;
   text-align: center;
-  border: .1rem solid black;
+  font-size: 3rem;
+  font-weight: 600;
+  color: white;
+  box-shadow: inset 0 0 10rem rgba(0, 0, 0, .2);
+  text-shadow: 0 -.05rem 0rem #777;
+  border: .05rem solid black;
   box-sizing: border-box;
-}
-.day:before {
-  content: '';
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  background: linear-gradient(-5deg, rgba(0,0,0,0.2), rgba(255,255,255,0.2));
-  mix-blend-mode: overlay;
-  touch-action: none;
-  z-index: 1;
+
+  &:before {
+    content: '';
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    background: linear-gradient(-5deg, rgba(0,0,0,0.25), rgba(255,255,255,0.3));
+    mix-blend-mode: overlay;
+    pointer-events: none;
+    z-index: 1;
+  }
+
+  & > div {
+    width: 100%;
+    margin: auto 0;
+  }
+
+  & .icon {
+    font-size: 5rem;
+  }
+
+  .list {
+    font-size: 2rem;
+    text-align: left;
+    margin: 1rem 0 1.75rem 0;
+
+    & .challenge {
+      border-top: .1rem solid rgba(0, 0, 0, .2);
+      border-bottom: .1rem solid rgba(0, 0, 0, .2);
+      padding: .5rem 2rem;
+      margin-bottom: -.1rem;
+      background: linear-gradient(-5deg, rgba(0,0,0,0.05), rgba(0,0,0,0));
+      cursor: pointer;
+    }
+  }
 }
 
-.day > div {
-  margin: auto;
+.fin {
+  display: block;
+  width: calc(100% - 4rem);
+  font-size: 2.5rem;
+  background: none;
+  color: white;
+  font-weight: 700;
+  padding: .5rem;
+  margin: 0 2rem;
+  box-shadow: 0 .05rem 0rem #777;
+  border: .2rem solid white;
+  transition: .2s all;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background: rgba(0, 0, 0, .2);
+    transition: .2s all;
+  }
 }
+
 </style>
