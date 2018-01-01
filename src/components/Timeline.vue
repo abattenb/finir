6d62a4a0f10f2ddea70675bfb53163f5add1c0a3
Andrew Battenburg <abattenb@gmail.com>
2017-12-31 20:46:03 -0800

    Initial commit

---
+++ src/components/Timeline.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="timeline">
+    <div v-for="day in data" :key="day.date" class="day">
+      {{day.date}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  data() {
+    return {
+      data: [
+        {
+          date: '2017-12-31',
+          complete: false,
+          color: '#ef5350',
+        },
+        {
+          date: '2017-12-30',
+          complete: false,
+          color: '#26A69A',
+        },
+        {
+          date: '2017-12-29',
+          complete: false,
+          color: '#7E57C2',
+        },
+      ],
+    };
+  },
+  methods() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.timeline {
+  display: flex;
+}
+
+.day {
+  
+  border: .1rem solid black;
+}
+</style>
